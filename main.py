7867aa1953324ed2692c6bd80bbff1910b6d4f9f
StepanPetru <283230@vutbr.cz>
2026-04-20 13:25:36 +0200

    odevzdani12345

--- main.py
+++ main.py
@@ -32,6 +32,31 @@ def bubble_sort(numbers):
     plt.show()
 
     return numbers
+class StudentsGrades:
+    def __init__(self, scores):
+        self.scores = scores
+
+    def get_by_index(self, index):
+        return self.scores[index]
+
+    def count(self):
+        return len(self.scores)
+
+    def get_grade(self, index):
+        score = self.scores[index]
+
+        if score >= 90:
+            return "A"
+        elif score >= 80:
+            return "B"
+        elif score >= 70:
+            return "C"
+        elif score >= 60:
+            return "D"
+        elif score >= 50:
+            return "E"
+        else:
+            return "F"
 
 
 def main():
