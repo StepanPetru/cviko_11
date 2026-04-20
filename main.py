0745a6ddc0472b698c58c6eb5f41984c00fd3afc
StepanPetru <283230@vutbr.cz>
2026-04-20 13:31:11 +0200

    odevzdani_velke_finale

--- main.py
+++ main.py
@@ -32,9 +32,12 @@ def bubble_sort(numbers):
     plt.show()
 
     return numbers
+
+
 class StudentsGrades:
     def __init__(self, scores):
         self.scores = scores
+        self._sorted_scores = None
 
     def get_by_index(self, index):
         return self.scores[index]
@@ -58,30 +61,43 @@ class StudentsGrades:
         else:
             return "F"
 
+    def find(self, value):
+        indexes = []
+
+        for i in range(len(self.scores)):
+            if self.scores[i] == value:
+                indexes.append(i)
+
+        return indexes
+
+    def get_sorted(self):
+        scores = self.scores.copy()
+        n = len(scores)
+
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if scores[j] > scores[j + 1]:
+                    scores[j], scores[j + 1] = scores[j + 1], scores[j]
+
+        return scores
+
 
 def main():
+    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+
+    print("count:", results.count())
+
+    for i in range(results.count()):
+        print("Student", i, ":", results.get_by_index(i), "-", results.get_grade(i))
+
+    print("find 100:", results.find(100))
+    print("find 50:", results.find(50))
+    print("find 77:", results.find(77))
+
+    print("sorted:", results.get_sorted())
+
+    print("bubble animation:")
     bubble_sort(random_numbers(10))
-
-
-if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def main():
-    print("Hello from cviko-11!")
 
 
 if __name__ == "__main__":
